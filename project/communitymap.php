3f259a72290b70c95934a917b563ca97a9c2e334
Derek Lai <Dpkl18@aol.com>
2015-09-16 01:22:06 -0500

    Added Geocoding  and Reverse Geocoding
    Added the ability for the map use an address to get the latitude and
    longitude, as well as use the latitude & longitude to get an address
    (Address format needs to be more specific)

--- project/communitymap.php
+++ project/communitymap.php
@@ -20,50 +20,124 @@ body, html {
 
  <!-- Load In Google Maps -->
 <script>
-function initialize() {
-  var mapProp = {
-    //Need To Center at Center of Neighborhood
-    //Decides default center of Google Maps
-    center:new google.maps.LatLng(41.7605556, -88.3200),
-    zoom:12,
-    mapTypeId:google.maps.MapTypeId.ROADMAP
+var map;
+//Sets the default center of the Map
+//Should change to Community Location (if set)
+var myCenter=new google.maps.LatLng(41.7605556, -88.3200);
+
+function initialize()
+{
+var mapProp = {
+  center:myCenter,
+  zoom:10,
+  mapTypeId:google.maps.MapTypeId.ROADMAP
   };
-  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+  map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+  google.maps.event.addListener(map, 'click', function(event) {
+    placeMarker(event.latLng);
+  });
+
+  var geocoder = new google.maps.Geocoder();
+
+  var infowindow = new google.maps.InfoWindow;
+
+  document.getElementById('submitG').addEventListener('click', function() {
+    geocodeLatLng(geocoder, map, infowindow);
+  });
+
+  document.getElementById('submitA').addEventListener('click', function() {
+    geocodeAddress(geocoder, map);
+  });
+
 }
-google.maps.event.addDomListener(window, 'load', initialize);
 
-var myGeographicCoordinates = new LatLng(41.7605556, -88.3200);
+//Need to put this into a loop to pipe in Addresses from Database
+function geocodeAddress(geocoder, resultsMap) {
+  var address = document.getElementById('address').value;
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === google.maps.GeocoderStatus.OK) {
+      resultsMap.setCenter(results[0].geometry.location);
 
-// Zoom to 14 when clicking on marker
-google.maps.event.addListener(marker,'click',function() {
-  map.setZoom(14);
-  map.setCenter(marker.getPosition());
+//Sets a Marker at the locations in the Geocoder search
+      var marker = new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location
+      });
+      
+//Creates an info Window showing Latitude and Longitude
+      var infowindow = new google.maps.InfoWindow({
+        content: 'Getting Location By Address: </br>' + 'Latitude: ' + results[0].geometry.location.lat() + '<br>Longitude: ' + results[0].geometry.location.lng()
+      });
+
+//Sets the infor window on the marker 
+      infowindow.open(map,marker);
+
+// Zoom to 15 when clicking on marker and opens the infow window if its closed
+      google.maps.event.addListener(marker,'click',function() {
+        map.setZoom(12);
+        map.setCenter(marker.getPosition());
+        infowindow.open(map,marker);
+      });
+
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
   });
+}
 
-//Places a marker when clicking
-google.maps.event.addListener(map, 'click', function(event) {
-  placeMarker(event.latLng);
+
+function geocodeLatLng(geocoder, map, infowindow) {
+  var input = document.getElementById('latlng').value;
+  var latlngStr = input.split(',', 2);
+  var latlng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
+  geocoder.geocode({'location': latlng}, function(results, status) {
+    if (status === google.maps.GeocoderStatus.OK) {
+      if (results[1]) {
+        map.setZoom(11);
+        var marker = new google.maps.Marker({
+          position: latlng,
+          map: map
+        });
+        infowindow.setContent( 'Getting Location By Geocode: </br>' + results[1].formatted_address);
+        infowindow.open(map, marker);
+      } else {
+        window.alert('No results found');
+      }
+    } else {
+      window.alert('Geocoder failed due to: ' + status);
+    }
   });
+}
+
 
 function placeMarker(location) {
+
+//Adds a Marker where the User Clicks
   var marker = new google.maps.Marker({
     position: location,
     map: map,
   });
 
-  //Opens an info window on marker
+//Adds an Info Window on the Marker
+//Showing Latitude and Logitude
+//Need to take Latlng into database
   var infowindow = new google.maps.InfoWindow({
-    content: 'Latitude: ' + location.lat() +
-    '<br>Longitude: ' + location.lng()
+    content: 'Latitude: ' + location.lat() + '<br>Longitude: ' + location.lng()
   });
-
-  //Opens the info window when clicking a marker
-  google.maps.event.addListener(marker, 'click', function() {
   infowindow.open(map,marker);
-  });
 
-  
+// Zoom to 15 when clicking on marker
+  google.maps.event.addListener(marker,'click',function() {
+    map.setZoom(12);
+    map.setCenter(marker.getPosition());
+    infowindow.open(map,marker);
+    });
+
 }
+
+google.maps.event.addDomListener(window, 'load', initialize);
 
 </script>
 
@@ -72,7 +146,19 @@ function placeMarker(location) {
 <body>
 
   <div class="container" style="width:100%; height:95%;">
-    <div class="col-sm-4" style="background-color: #19A3FF; height:100%;"> A side window here</div>
+  <!--Side Panel Div-->
+    <div class="col-sm-4" style="background-color: #19A3FF; height:100%;">
+      <div>
+        <input id="address" type="textbox" value="Aurora, IL">
+        <input id="submitA" type="button" value="Geocode">
+      </div>
+      <div>
+        <input id="latlng" type="text" value="41.7605849,-88.3200715">
+        <input id="submitG" type="button" value="Reverse Geocode">
+      </div>
+    </div>
+
+  <!--Google Map Div-->
     <div class="col-sm-8" id="googleMap" style="position: relative; height:100%;" ></div>
   </div>
 
